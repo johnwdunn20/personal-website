336413074f958558007e777df706c4a5499b4877
John Dunn <johnwdunn20@gmail.com>
2024-02-03 14:54:35 -0800

    added additional logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,17 @@ export default function Home() {
   const productData = [
     {
       title:"React Query Rewind",
-      description: "React Query Rewind allows developers using the state management tool React Query to time travel state directly in their browser. Simply install the Chrome extension, NPM Package, and place the custom component as close to the root of your application as possible",
-      technicalDescription: "Built with TypeScript, React, & MaterialUI",
+      description: "React Query Rewind allows developers using the state management tool React Query to time travel state directly in their browser",
+      bullets: [
+        'Install the NPM package and Chrome Extension',
+        'Placed the custom component next to the root of your application',
+        'Happy debugging',
+      ],
+      technologies: [
+        'react',
+        'typescript',
+        'materialui',
+      ],
       website: "https://www.reactqueryrewind.com/",
       github: "https://github.com/oslabs-beta/react-query-rewind",
       npm: "https://www.npmjs.com/package/react-query-rewind",
@@ -42,7 +51,8 @@ export default function Home() {
             <Product
               title={product.title}
               description={product.description}
-              technicalDescription={product.technicalDescription}
+              bullets={product.bullets}
+              technologies={product.technologies}
               website={product.website}
               websiteSVG={product.websiteSVG}
               github = {product.github}
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -3,11 +3,15 @@ import Image from "next/image";
 import placeHolderImage from "@assets/grocery.png";
 import github from "@assets/github.svg";
 import javascript from '@assets/technologies/javascript-logo.svg';
+import typescript from '@assets/technologies/typescript-logo.svg';
+import reactLogo from '@assets/technologies/react-logo.svg';
+import materialUILogo from '@assets/technologies/materialui-logo.svg';
 
 type ProductType = {
   title: string;
   description: string;
-  technicalDescription?: string;
+  bullets?: string[];
+  technologies?: string[];
   darkBackground: boolean;
   website?: string;
   websiteSVG?: string;
@@ -24,7 +28,8 @@ type ProductType = {
 const Product: React.FC<ProductType> = ({
   title,
   description,
-  technicalDescription,
+  bullets,
+  technologies,
   darkBackground,
   website,
   websiteSVG,
@@ -47,32 +52,92 @@ const Product: React.FC<ProductType> = ({
     >
       {/* Text and Images */}
       <div className="flex justify-center flex-wrap gap-4 md:gap-8 lg:gap-12">
-        <article className="flex flex-col justify-around w-full md:w-2/3 xl:w-1/3 mx-4 sm:mx-2 md:mx-0">
+        <article className="flex flex-col w-full md:w-2/3 xl:w-1/3 mx-4 sm:mx-2 md:mx-0">
           {/* Text */}
           <h1 className="text-4xl font-bold text-center w-full bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
             {title}
           </h1>
-          <p className="w-full my-4 text-xl">{description}</p>
-          <p className="w-full my-4 italic self-center ml-12 text-2xl bg-gradient-to-r from-purple-600 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent">
+          <p className="w-full my-4 text-xl text-slate-200">{description}</p>
+          <ul className="ml-4 mt-4 list-none">
+            {bullets && bullets.map((bullet, index) => <li
+            key={index}
+            className="text-lg m-2 text-slate-200 relative pl-5 before:absolute before:bg-orange-500 before:rounded-full before:w-2 before:h-2 before:left-0 before:top-2 before:content-['']"
+            >{bullet}</li>)}
+          </ul>
+          {/* <p className="w-full my-4 italic self-center ml-12 text-2xl bg-gradient-to-r from-purple-600 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent">
             Built with:
-          </p>
-          <div className="flex justify-around">
-            <a
-              href="https://www.javascript.com/"
-              target="_blank"
-              className="transform hover:scale-110 transition duration-300 ease-in-out"
-            >
-              <Image
-                src={javascript}
-                alt="JavaScript"
-                width={140}
-                height={50}
-                className="rounded-lg shadow-2xl hover:shadow-inner transition-shadow duration-200 ease-in-out"
-              />
-            </a>
-            <div>TypeScript</div>
-            <div>React</div>
-            <div>MaterialUI</div>
+          </p> */}
+          
+          {/* Technologies */}
+          <div className="flex flex-col justify-end h-full mt-4">
+            <div className="flex justify-around flex-wrap">
+
+              {/* Javascript */}
+              {technologies?.includes("javascript") && 
+                <a
+                  href="https://www.javascript.com/"
+                  target="_blank"
+                  className="transform hover:scale-110 transition duration-300 ease-in-out"
+                >
+                  <Image
+                    src={javascript}
+                    alt="JavaScript"
+                    width={140}
+                    height={50}
+                    className="rounded-lg shadow-2xl hover:shadow-inner transition-shadow duration-200 ease-in-out"
+                  />
+                </a>
+              }
+              {/* TypeScript */}
+              {technologies?.includes("typescript") && 
+                <a
+                  href="https://www.typescriptlang.org/"
+                  target="_blank"
+                  className="transform hover:scale-110 transition duration-300 ease-in-out"
+                >
+                  <Image
+                    src={typescript}
+                    alt="TypeScript"
+                    width={140}
+                    height={50}
+                    className="rounded-lg shadow-2xl hover:shadow-inner transition-shadow duration-200 ease-in-out"
+                  />
+                </a>
+              }
+              {/* React */}
+              {technologies?.includes("react") && 
+                <a
+                  href="https://react.dev/"
+                  target="_blank"
+                  className="transform hover:scale-110 transition duration-300 ease-in-out"
+                >
+                  <Image
+                    src={reactLogo}
+                    alt="React"
+                    width={140}
+                    height={50}
+                    className="rounded-lg shadow-2xl hover:shadow-inner transition-shadow duration-200 ease-in-out"
+                  />
+                </a>
+              }
+              {/* MaterilUI */}
+              {technologies?.includes("materialui") && 
+                <a
+                  href="https://mui.com/"
+                  target="_blank"
+                  className="transform hover:scale-110 transition duration-300 ease-in-out"
+                >
+                  <Image
+                    src={materialUILogo}
+                    alt="MaterialUI"
+                    width={140}
+                    height={50}
+                    className="rounded-lg shadow-2xl hover:shadow-inner transition-shadow duration-200 ease-in-out"
+                  />
+                </a>
+              }
+
+            </div>
           </div>
         </article>
 
