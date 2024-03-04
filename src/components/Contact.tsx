26e4a3f72c79be4d04d4b5ff828949e71b987cd7
John Dunn <johnwdunn20@gmail.com>
2024-03-03 20:58:46 -0700

    added drop shadow to all titles

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,7 +22,12 @@ const Contact = () => {
 
   return (
     <section id="contact" className="bg-slate-800 w-full">
-      <h1 className="text-center font-bold text-6xl mt-4 bg-gradient-to-r from-purple-600 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent">
+      <h1
+        className="text-center font-bold text-6xl mt-4 bg-gradient-to-r from-purple-600 via-pink-500 to-fuchsia-500 bg-clip-text text-transparent"
+        style={{
+          filter: "drop-shadow(0 10px 10px rgba(0,0,0,1.5))",
+        }}
+      >
         Contact Me
       </h1>
       {/* LinkedIn and Github */}
