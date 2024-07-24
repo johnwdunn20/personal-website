18124d2fce9627c7a2726a1bfd7d91d4efd3e24b
John Dunn <johnwdunn20@gmail.com>
2024-07-23 17:25:36 -0700

    changes

--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -71,7 +71,7 @@ const FloatingNav = ({
         className={cn(
           "hidden sm:flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-white/[0.2] rounded-full bg-slate-900 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-4 pl-4 py-2  items-center justify-center space-x-4 bg-opacity-70",
           className
-        )}
+        )} 
       >
         {navItems.map((navItem: any, idx: number) => (
             // <button
